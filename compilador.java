0a2daf1b375c85f5c34936203f607b2bdee0e695
abimael93 <chris.abimael93@gmail.com>
2015-02-17 22:13:56 -0600

    Se agregó funcionalidad para abrir archivos
    El programa tiene detalles por los cuales no funciona.

--- compilador.java
+++ compilador.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.io.*;
 
 public class compilador {
         
@@ -9,10 +10,47 @@ public class compilador {
     
     
     public static void main(String[] args) {
-    	 int cont =10;
+		int tamanioLinea, i, sigSimbolo = -1 ;
+		FileReader fr;
+		BufferedReader br;
+		String linea = "";
+        
         lexico lex= new lexico();
-       	do{
-        lex.recibeSimbolo();
-       	}while(cont<100);
+        
+		File archivo = new File("entrada.txt");
+		try{
+			//Código
+			fr = new FileReader (archivo);
+			br = new BufferedReader (fr);
+			while( (linea = br.readLine() ) != null && sigSimbolo != lex.E ) {
+				//System.out.println("Entró!");
+				tamanioLinea = linea.length();
+				System.out.println("Tamaño de la línea: " + tamanioLinea );
+				sigSimbolo = -1;
+				for( i = 0, lex.inicioSim = lex.finSim = 0 ; lex.finSim < tamanioLinea && sigSimbolo != lex.E && i < 2; lex.inicioSim = lex.finSim , i++) {
+					//System.out.println("Entró1!");
+					sigSimbolo = lex.recibeSimbolo( lex.siguienteSimbolo( linea , tamanioLinea ) );
+					System.out.println("Puntero: " + lex.finSim );
+					//System.out.println("Entró2!");
+					System.out.print( sigSimbolo + ": " );
+					lex.imprimeEstado( sigSimbolo );
+					//break;
+				}
+				break;
+			}
+			if(sigSimbolo != lex.E ) {
+				System.out.println("1");
+			} else {
+				System.out.println("0");
+			}
+			
+		} catch (FileNotFoundException fnfe){
+			//fnfe.printStackTrace();
+			System.out.println("Error, no se encontró el archivo especificado!");
+		} catch (IOException ioe){
+			ioe.printStackTrace();
+		}
+		
+		//System.out.println("hola!");
     }
 }
--- lexico.java
+++ lexico.java
@@ -5,52 +5,89 @@ public class lexico extends compilador {
     	
     }
     
-  		String Cadena="";
-    	int E=100;
-    	int[][] tabla=
-    		{
-    			{E,1,2,3,5,5,E,7,8,E,9},
-    			{E,E,E,E,E,E,E,E,E,E,E},
-    			{E,E,E,E,E,E,E,E,E,E,E},
-    			{E,E,E,E,4,E,E,E,E,E,E},
-    			{E,E,E,E,E,E,E,E,E,E,E},
-    			{E,E,E,E,6,E,E,E,E,E,E},
-    			{E,E,E,E,E,E,E,E,E,E,E},
-    			{E,E,E,E,E,E,E,7,7,E,E},
-    			{E,E,E,E,E,E,E,E,8,9,E},
-    			{E,E,E,E,6,E,E,E,10,E,E},
-    			{E,E,E,E,E,E,E,E,10,E,E},
-    		};
+	String Cadena 	= "";
+	int E			= 100;
+	int[][] tabla 	=
+		{
+			{E,1,2,3,5,5,E,7,8,E,9},
+			{E,E,E,E,E,E,E,E,E,E,E},
+			{E,E,E,E,E,E,E,E,E,E,E},
+			{E,E,E,E,4,E,E,E,E,E,E},
+			{E,E,E,E,E,E,E,E,E,E,E},
+			{E,E,E,E,6,E,E,E,E,E,E},
+			{E,E,E,E,E,E,E,E,E,E,E},
+			{E,E,E,E,E,E,E,7,7,E,E},
+			{E,E,E,E,E,E,E,E,8,9,E},
+			{E,E,E,E,6,E,E,E,10,E,E},
+			{E,E,E,E,E,E,E,E,10,E,E},
+		};
+	int inicioSim, finSim;
     		
-    public void recibeSimbolo()
-    {
-    	int i,estado=0,valor=0;
+    public int recibeSimbolo( String cadena ) {
+    	int i, estado = 0, valor = 0;
     	char c;
-    	System.out.println("Ingresa una cadena\n");
-    	Cadena=Leer.nextLine();
-    	estado=analizaReservada(Cadena);
-    	if(estado<1)
-    	{
-	    	for(i=0;i<Cadena.length() && estado!=E;i++)
-	    	{
-	    		c=Cadena.charAt(i);
-	    		valor=analizaCaracter(c);
-	    		estado=tabla[estado][valor];
+    	//System.out.println("Ingresa una cadena\n");
+    	//Cadena=Leer.nextLine();
+    	estado = analizaReservada( cadena );
+    	
+    	if( estado < 1 ) {
+	    	for( i = 0 ; i < cadena.length() && estado != E; i++ ) {
+	    		c		= cadena.charAt( i );
+	    		valor 	= analizaCaracter( c );
+	    		estado	= tabla[estado][valor];
 	    	}
     	}
     	
-    	imprimeEstado(estado);
+    	return estado;
+    	//imprimeEstado( estado );
     }
     
-    public int analizaReservada(String palabra)
-    {
+    public String siguienteSimbolo( String linea, int tam ) {
+    	String cadena = "";
+    	char c;
+    	
+    	while( ( linea.charAt( finSim ) == ' ' ) && finSim < tam ) {
+    		finSim++;
+    		inicioSim++;
+    	}
+    	
+    	//System.out.println( "Entró3!" );
+    	if( finSim < tam ) {
+    		if()
+    		while( ( finSim < tam && !esSeparador( linea.charAt( finSim ) ) )  ) {
+	    		c 		= linea.charAt( finSim );
+	    		cadena += c;
+	    		finSim++;
+	    	}
+    	}
+    	
+    	System.out.println("Entró!4: " + cadena );
+    	
+    	//Regresamos el substring
+    	return cadena;
+    }
+    
+    public boolean esSeparador( char c ) {
+    	switch( c ) {
+    		case '>': case '<': case '!': case '=': case ' ':
+    		case '+': case '-': case '*': case '/': case '(':
+    		case ')':
+    			return true;
+    	}
+    	return false;
+    }
+    
+    public int analizaReservada(String palabra) {
     	int valor=0;
     	switch(palabra)
     	{
     		case "and":case "or":case "not":
     			valor=20;
     			break;
-    		case "if": case "then":case "while":case "else":case "do":case "for":case "begin":case "end":case "writeln":case "readln":case "var":case "program":case "char":case "integer":case "byte":case "Real":case "Boolean":case "String": case "word":case "longint":case "shortint":    			valor=21;
+    		case "if": case "then":case "while":case "else":case "do":case "for":case "begin":case "end":
+    		case "writeln":case "readln":case "var":case "program":case "char":case "integer":case "byte":
+    		case "Real":case "Boolean":case "String": case "word":case "longint":case "shortint":
+    			valor=21;
     			break;
     		case ".":
     			valor=22;
@@ -61,47 +98,46 @@ public class lexico extends compilador {
     	
     }
     
-    public void imprimeEstado( int estado ) 
-    	{
-		    switch ( estado ) {
-		        case 1:
-		            System.out.println("Delimitador");
-		            break;
-		        case 2:
-		            System.out.println("Operador Aritmético");
-		            break;
-		        case 3:
-		            System.out.println("Declaracion");
-		            break;
-		        case 4:
-		            System.out.println("Operador Asignación");
-		            break;
-		        case 5: case 6:
-		            System.out.println("Operador Relacional");
-		            break;
-		        case 7:
-		            System.out.println("Identificador");
-		            break;
-		        case 8:
-		            System.out.println("Entero");
-		            break;
-		        case 10:
-		            System.out.println("Real");
-		            break;
-		        case 20:
-		            System.out.println("Operador Lógico");
-		            break;
-		        case 21:
-		            System.out.println("Palabra Reservada");
-		            break;
-		        case 22:
-		            System.out.println("Punto");//Final del Programa
-		            break;
-		        default:
-		            System.out.println("Error");
-		            break;
-    }
-}
+    public void imprimeEstado( int estado ) {
+	    switch ( estado ) {
+	        case 1:
+	            System.out.println("Delimitador");
+	            break;
+	        case 2:
+	            System.out.println("Operador Aritmético");
+	            break;
+	        case 3:
+	            System.out.println("Declaracion");
+	            break;
+	        case 4:
+	            System.out.println("Operador Asignación");
+	            break;
+	        case 5: case 6:
+	            System.out.println("Operador Relacional");
+	            break;
+	        case 7:
+	            System.out.println("Identificador");
+	            break;
+	        case 8:
+	            System.out.println("Entero");
+	            break;
+	        case 10:
+	            System.out.println("Real");
+	            break;
+	        case 20:
+	            System.out.println("Operador Lógico");
+	            break;
+	        case 21:
+	            System.out.println("Palabra Reservada");
+	            break;
+	        case 22:
+	            System.out.println("Punto");//Final del Programa
+	            break;
+	        case E:
+	            System.out.println("Error");
+	            break;
+    	}
+	}
     public int analizaCaracter(char caracter)
     {
     	int valor=0;
